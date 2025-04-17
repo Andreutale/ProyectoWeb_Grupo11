ead16a70d095ad142c2d29fd04bb0f89f6070b28
ferran <ferransansaloni@gmail.com>
2025-04-17 14:27:29 +0200

    Estructura básica
    Creados los ficheros necesarios para tener la estructura básica del proyecto

---
+++ src/html/includes/js/prueba.js
@@ -0,0 +1,4 @@
+// Quien necesite usar algun fichero javascript puede cambiar el nombre de este y usarlo
+
+// Se ha creado únicamente para que aparezca la carpeta en github al hacer commit, si esta vacía ni se me
+// crea la carpeta al hacer commit <3
